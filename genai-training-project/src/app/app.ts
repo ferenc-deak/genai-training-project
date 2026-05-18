ddedc86fa4527c8f0bd2cd32d1b8bf216b7129af
Ferenc Deak <ferenc.deak@accesa.eu>
2026-05-18 13:30:09 +0300

    added rag and AI search result to be displayed

--- genai-training-project/src/app/app.ts
+++ genai-training-project/src/app/app.ts
@@ -12,48 +12,66 @@ import { HttpClient } from '@angular/common/http';
 })
 export class App {
   prompt = '';
-  response = '';
+
+  ragResponse = '';
+  aiResponse = '';
+
   loading = false;
+
+  private completedRequests = 0;
 
   constructor(private http: HttpClient) {}
 
-  send() {
-    this.loading = true;
-    this.response = '';
-
-    this.http
-      .post<any>('http://127.0.0.1:8000/chat', {
-        prompt: this.prompt,
-      })
-      .subscribe({
-        next: (res) => {
-          this.response = res.response;
-          this.loading = false;
-        },
-        error: () => {
-          this.response = 'Error calling chat';
-          this.loading = false;
-        },
-      });
-  }
-
   sendRAG() {
-    this.loading = true;
-    this.response = '';
-
     this.http
       .post<any>('http://127.0.0.1:8000/ask', {
         question: this.prompt,
       })
       .subscribe({
         next: (res) => {
-          this.response = res.answer || res.response;
-          this.loading = false;
+          this.ragResponse = res.answer || res.response;
+          this.checkDone();
         },
         error: () => {
-          this.response = 'Error calling RAG';
-          this.loading = false;
+          this.ragResponse = 'Error calling RAG';
+          this.checkDone();
         },
       });
+  }
+
+  sendAI() {
+    this.http
+      .post<any>('http://127.0.0.1:8000/chat', {
+        prompt: this.prompt,
+      })
+      .subscribe({
+        next: (res) => {
+          this.aiResponse = res.response;
+          this.checkDone();
+        },
+        error: () => {
+          this.aiResponse = 'Error calling chat';
+          this.checkDone();
+        },
+      });
+  }
+
+  combineBothAPICall() {
+    this.loading = true;
+    this.ragResponse = '';
+    this.aiResponse = '';
+
+    this.completedRequests = 0;
+
+    this.sendRAG();
+    this.sendAI();
+  }
+
+  private checkDone() {
+    this.completedRequests++;
+
+    if (this.completedRequests === 2) {
+      this.loading = false;
+    }
   }
 }
